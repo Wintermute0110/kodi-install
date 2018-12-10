e6f70b67a215570e363fe216e10ec01ff363c0b8
Wintermute0110 <wintermute0110@gmail.com>
2018-12-11 00:23:46 +0900

    Clean binary addons build directory.

---
+++ purge-addons-build-directory.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Completely deletes the addons build directory inside the Kodi source directory.
+#
+rm -rf /home/kodi/kodi-source/tools/depends/target/binary-addons/.installed-native
+rm -rf /home/kodi/kodi-source/tools/depends/target/binary-addons/native/
