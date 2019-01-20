0d92381f91dfc7582aac2337d968e8a125fbdde9
Wintermute0110 <wintermute0110@gmail.com>
2019-01-20 13:09:05 +0900

    Add support for Ubuntu Xenial Xerus.

--- configure-kodi-debian-unstable.sh
+++ configure-kodi-debian-unstable.sh
@@ -5,5 +5,5 @@
 mkdir -p /home/kodi/kodi-build
 cd /home/kodi/kodi-build
 
-# cmake ../kodi-source -DCMAKE_INSTALL_PREFIX=/home/kodi/bin-kodi -DENABLE_INTERNAL_FLATBUFFERS=ON -DVERBOSE=ON
+# Options: -DVERBOSE=ON
 cmake ../kodi-source -DCMAKE_INSTALL_PREFIX=/home/kodi/bin-kodi -DENABLE_INTERNAL_FLATBUFFERS=ON
