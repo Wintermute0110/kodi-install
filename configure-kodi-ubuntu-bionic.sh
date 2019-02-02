9e5934e5d0dbd9a69260e373b4e59d0c11331c93
Wintermute0110 <wintermute0110@gmail.com>
2019-02-02 22:42:44 +0900

    New building files for Ubuntu Bionic 18.04.

---
+++ configure-kodi-ubuntu-bionic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Creates build directory and configures Kodi build.
+
+mkdir -p /home/kodi/kodi-build
+cd /home/kodi/kodi-build
+
+# Options: -DVERBOSE=ON
+cmake ../kodi-source -DCMAKE_INSTALL_PREFIX=/home/kodi/bin-kodi -DENABLE_INTERNAL_FLATBUFFERS=ON
