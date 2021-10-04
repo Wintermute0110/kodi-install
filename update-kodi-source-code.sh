753b233f33bc4c97169694b744d6836bf2db0ea6
Wintermute0110 <wintermute0110@gmail.com>
2021-10-04 19:50:59 +0900

    Fix problem when updating source code.

--- update-kodi-source-code.sh
+++ update-kodi-source-code.sh
@@ -4,6 +4,9 @@ current_dir=`pwd`
 echo "Current directory $current_dir"
 echo "Kodi source directory $KODI_SOURCE_DIR"
 cd $KODI_SOURCE_DIR
+# Restore this file. Otherwise `git checkout` will fail because local changes
+# will be overwritten error. It is safe to execute `git restore` multiple times.
+git restore cmake/addons/bootstrap/repositories/binary-addons.txt
 git checkout master
 git pull --all
 cd $current_dir
